ed4fbdf45ac94e88bd9b6010e680e463ca5e9152
Heiko Voigt <heiko.voigt@jimdo.com>
2023-08-24 17:09:46 +0200

    adapt treesitter selection keys

--- init.lua
+++ init.lua
@@ -328,10 +328,10 @@ require('nvim-treesitter.configs').setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
+      init_selection = '<c-i>',
+      node_incremental = '<c-i>',
       scope_incremental = '<c-s>',
-      node_decremental = '<M-space>',
+      node_decremental = '<c-backspace>',
     },
   },
   textobjects = {
