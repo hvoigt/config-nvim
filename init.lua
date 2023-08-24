2890da839b8ac22759223afef8dfcce42752ca31
Heiko Voigt <heiko.voigt@jimdo.com>
2023-08-24 17:11:18 +0200

    Revert "add debug log level for lsp"
    This reverts commit 6b62b0ea0f802bb2e57dfacc86f58b3fe7f605e5.
    
    It allowed us to debug an issue with our ltex-ls integration with
    neovim.

--- init.lua
+++ init.lua
@@ -43,8 +43,6 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
-vim.lsp.set_log_level("debug")
-
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
