96885e10f8b6335cde94d5e2e8171f26f9902a03
Heiko Voigt <heiko.voigt@jimdo.com>
2025-04-08 07:51:53 +0200

    ignore files in .git when grepping

--- init.lua
+++ init.lua
@@ -349,6 +349,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    vimgrep_arguments = { 'rg', '--hidden', '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '-g', '!.git' },
     mappings = {
       i = {
         ['<C-u>'] = false,
