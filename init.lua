91133e10ea8ade09f08571f5af8f4e5a38201d01
Heiko Voigt <heiko.voigt@jimdo.com>
2025-04-08 07:52:41 +0200

    add C-/ for accepting copilot suggestions
    Tab does multiple autocompletions. To directly choose the copilot
    suggestion we need an extra key.

--- init.lua
+++ init.lua
@@ -452,6 +452,11 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Copilot
+vim.keymap.set('i','<C-/>', 'copilot#Accept("\\<CR>")', {
+  expr = true, replace_keycodes = false
+})
+
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
