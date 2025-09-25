5d57ea01ea49331bab5b1dc8bdd849cdc9aaebea
Heiko Voigt <heiko.voigt@jimdo.com>
2025-09-25 15:18:00 +0200

    add copilot SSL config

--- init.lua
+++ init.lua
@@ -43,6 +43,9 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
+vim.g.copilot_proxy_strict_ssl = false
+vim.env.NODE_EXTRA_CA_CERTS = vim.fn.expand("~/.config/nvim/Cloudflare_CA.pem")
+
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
